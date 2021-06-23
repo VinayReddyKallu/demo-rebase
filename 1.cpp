55f5f5672c2905a28d9d4d12e637da33fb0d8d5b
vinayreddykallu <vinayreddykallu@gmail.com>
2021-06-23 22:46:19 +0530

    initial commit

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void add() {
+    cout<<"This is adder function"<<endl;
+    cout<<"We can add 2 integers"<<endl;
+}
+
+
+
+int main(){
+  
+    add();
+    return 0;
+}
